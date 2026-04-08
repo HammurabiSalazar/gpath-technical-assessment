c851929f9bf1e5158c6f39114e6ad78c83ee8d55
Dominick-Hms <2436000692@utna.edu.mx>
2026-04-07 22:19:27 -0600

    chore: realign project structure and files

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>GPath: Gestión de Muestras de Tejido</h1>
+      <p>Entorno preparado. Próximo paso: El Formulario.</p>
+    </div>
+  )
+}
+
+export default App
